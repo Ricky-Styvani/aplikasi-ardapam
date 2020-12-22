a1e672abc0790a9c47ebb51f9942f7a82e1d9c62
Ricky-Styvani <rickystyvani@gmail.com>
2020-12-22 11:00:35 +0700

    datatable catat

---
+++ resources/js/views/petugas/catat/list_catat.vue
@@ -0,0 +1,39 @@
+<template>
+   <div class="card mt-0">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-8 align-self-center pd-4">
+                    <h3>
+                        Catat
+                    </h3>
+                </div>
+            </div>
+        </div>
+        <div class="card-body p-2">
+            <Table />
+            
+             
+        </div>
+       
+    </div>
+</template>
+
+<script>
+import Table from './datatable.vue';
+
+export default {
+    name: "listcatat",
+    components:{
+        Table
+    },
+    data () {
+        return {
+            
+        };
+    },
+       
+        
+
+    }
+</script>
+
